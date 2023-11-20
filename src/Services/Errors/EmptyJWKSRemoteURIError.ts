3b0239a26232a2e37b7369a8937492dc46deed61
Guillaume MODARD <guillaumemodard@gmail.com>
2023-11-20 23:30:14 +0100

    Improving errors names

--- src/Services/Errors/EmptyJWKSRemoteURIError.ts
+++ src/Services/Errors/EmptyJWKSRemoteURIError.ts
@@ -2,7 +2,7 @@ export class EmptyJWKSRemoteURIError extends Error
 {
     constructor() {
         super("The public JWK Set URI is empty. It can be due to a misconfiguration on your server. Did you export " +
-            "PBL_AUTH_PUBLIC_KEYS_URI environment variable on your system or on your .env file ?");
+            "PBL_JWKS_REMOTE_URI environment variable on your system or on your .env file ?");
         this.name = "EmptyJWKSRemoteURIError"
     }
 }
