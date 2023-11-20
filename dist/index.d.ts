2307df8aaa77f77578ec5c80b67eb247da2d971f
Guillaume MODARD <guillaumemodard@gmail.com>
2023-11-20 10:09:40 +0100

    Support de l'ESM pour l'import export du module

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import AuthenticatedLicence from "./Models/AuthenticatedLicence";
-import PebbleAuthToken from "./Models/PebbleAuthToken";
-import User from "./Models/User";
-import { auth, authFromHttpHeaders } from "./Services/auth";
-export { AuthenticatedLicence, PebbleAuthToken, User, auth, authFromHttpHeaders };
