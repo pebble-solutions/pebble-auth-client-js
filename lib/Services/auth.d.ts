5a19f0e0ae76396b6be010bfeda09ffa7bff8237
Guillaume MODARD <guillaumemodard@gmail.com>
2023-11-21 10:23:21 +0100

    Compilation

--- lib/Services/auth.d.ts
+++ lib/Services/auth.d.ts
@@ -1,4 +1,6 @@
+/// <reference types="node" />
 import PebbleAuthTokenInterface from "../Interfaces/PebbleAuthTokenInterface";
+import { IncomingHttpHeaders } from "http";
 /**
  * Authenticate a provided token into and return a valid PebbleAuthToken object
  *
@@ -8,4 +10,4 @@ export declare function auth(token: string): Promise<PebbleAuthTokenInterface>;
 /**
  * Authenticate user using the HTTP Authorization header provided with the request
  */
-export declare function authFromHttpHeaders(): Promise<PebbleAuthTokenInterface>;
+export declare function authFromHttpHeaders(headers: IncomingHttpHeaders): Promise<PebbleAuthTokenInterface>;
