d785ca2572d463c2d06e6e4020f0bbdab015a84d
Guillaume MODARD <guillaumemodard@gmail.com>
2023-11-30 08:15:59 +0100

    finalize JSDoc

--- src/Interfaces/PebbleAuthTokenInterface.ts
+++ src/Interfaces/PebbleAuthTokenInterface.ts
@@ -3,28 +3,64 @@ import AuthenticatedLicenceInterface from "./AuthenticatedLicenceInterface";
 
 export default interface PebbleAuthTokenInterface {
 
-    sub: string
+    /**
+     * Application for which the token is generated
+     */
+    aud: string;
 
-    iss: string
+    /**
+     * Expiration timestamp
+     */
+    exp: number;
 
-    aud: string
+    /**
+     * Issued at time (timestamp)
+     */
+    iat?: number;
 
-    tid: string
+    /**
+     * Issuer : Licence ID that emit the token
+     */
+    iss: string;
 
-    roles?: string[]
+    /**
+     * From 1-6 : user level affected by the licence
+     */
+    lv?: number;
 
-    lv?: number
+    /**
+     * Display name for the user
+     */
+    name?: string;
 
-    name?: string
+    /**
+     * Roles attributed to the user
+     */
+    roles?: string[];
 
-    iat?: number
+    /**
+     * User email (used as username)
+     */
+    sub: string;
 
-    exp: number
+    /**
+     * Tenant ID : customer id, client id... that will consume resources
+     */
+    tid: string;
 
-    token: string
+    /**
+     * Token from which datas has been deserialized
+     */
+    token: string;
 
+    /**
+     * Should get the user who own the token
+     */
     getUser (): UserInterface
 
+    /**
+     * Should get the authenticated licence object described by the token
+     */
     getAuthenticatedLicence (): AuthenticatedLicenceInterface
 
 }
--- src/Interfaces/UserInterface.ts
+++ src/Interfaces/UserInterface.ts
@@ -1,13 +1,30 @@
 export default interface UserInterface {
 
+    /**
+     * Unique username. Most of the time, it is a email address
+     */
     username: string
 
+    /**
+     * Roles affected to the user
+     */
     roles?: string[]
 
+    /**
+     * From 1-6 : user level
+     */
     level?: number
 
+    /**
+     * Name of the user used for display
+     */
     displayName?: string
 
+    /**
+     * Should check if the user has the argument specified role.
+     *
+     * @param role          Role that must be checked
+     */
     hasRole (role: string): boolean
 
 }
