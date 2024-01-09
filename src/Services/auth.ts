b25a797e46e987a3124d4a8ea15408c899c30ae6
Guillaume MODARD <guillaumemodard@gmail.com>
2024-01-09 18:23:30 +0100

    Add options to auth fonction for claims verification

--- src/Services/auth.ts
+++ src/Services/auth.ts
@@ -1,5 +1,5 @@
 import PebbleAuthTokenInterface from "../Interfaces/PebbleAuthTokenInterface";
-import {jwtVerify, createLocalJWKSet} from "jose";
+import {jwtVerify, createLocalJWKSet, JWTVerifyOptions} from "jose";
 import {getTokenDataFromJWTPayload} from "./token";
 import PebbleAuthToken from "../Models/PebbleAuthToken";
 import {getJWKSet} from "./key";
@@ -10,13 +10,19 @@ import {EmptyTokenError} from "./Errors";
  * Authenticate a provided token into and return a valid PebbleAuthToken object
  *
  * @param token         Provided token
+ * @param options       JWTVerifyOptions from jose library. The resource API should verify the audience claim at lease.
+ *                      More documentation : https://github.com/panva/jose/blob/2cd1015ff7b11efdaa2af0a2026651b62c5e88ca/docs/interfaces/jwt_verify.JWTVerifyOptions.md
  */
-export async function auth(token: string): Promise<PebbleAuthTokenInterface>
+export async function auth(token: string, options?: JWTVerifyOptions): Promise<PebbleAuthTokenInterface>
 {
+    options = options || {}
+
+    options.typ = "at+jwt"
+
     const keySet = await getJWKSet()
     const JWKS = createLocalJWKSet(keySet)
 
-    const { payload } = await jwtVerify(token, JWKS)
+    const { payload } = await jwtVerify(token, JWKS, options)
 
     const tokenData = getTokenDataFromJWTPayload(payload, token)
 
@@ -31,14 +37,16 @@ export async function auth(token: string): Promise<PebbleAuthTokenInterface>
  * - Token content must start with "Bearer " string (ex : *Bearer full_token_string*)
  *
  * @param headers       All provided headers (including Authorization) in a IncomingHttpHeaders object
+ * @param options       JWTVerifyOptions from jose library. The resource API should verify the audience claim at lease.
+ *                      More documentation : https://github.com/panva/jose/blob/2cd1015ff7b11efdaa2af0a2026651b62c5e88ca/docs/interfaces/jwt_verify.JWTVerifyOptions.md
  *
  * @throws EmptyTokenError
  */
-export async function authFromHttpHeaders(headers: IncomingHttpHeaders): Promise<PebbleAuthTokenInterface>
+export async function authFromHttpHeaders(headers: IncomingHttpHeaders, options?: JWTVerifyOptions): Promise<PebbleAuthTokenInterface>
 {
     if (headers.authorization) {
         const token = headers.authorization.replace(/^Bearer /, "")
-        return await auth(token)
+        return await auth(token, options)
     }
 
     throw new EmptyTokenError()
